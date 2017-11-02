0d6b5a0eef81d38e40434d4afafc4b7873a641a0
yxd3549 <yxd3549@rit.edu>
2017-11-02 13:43:08 -0400

    Tested the DecisionTreeClassifier. Next, I will use real data.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Machine_learning
 Experimenting with sklearn following this tutorial: http://dataaspirant.com/2017/02/01/decision-tree-algorithm-python-with-scikit-learn/
+
+TODO: Convert the format of my data to be used by the Decision Tree
+FORMAT: OUTCOME, FEATURE1, FEATURE2,... FEATURE10
+
+Possible Features:
+
+Number of videos watched,
+Rating (Should give high percentage)
+Number of emotions
+Emotions boolean
+Previously watched
+Number of clicks
+Number of peaks
+Percentage of time that emotion was detected (Complex and unlikely to work)
+
+
--- practice.py
+++ practice.py
@@ -16,3 +16,23 @@ X = balance_data.values[:, 1:5]
 Y = balance_data.values[:,0]
 
 X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
+
+clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100, max_depth=3, min_samples_leaf=5)
+print(clf_gini.fit(X_train, y_train))
+
+clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100, max_depth=3, min_samples_leaf=5)
+clf_entropy.fit(X_train, y_train)
+
+clf_gini.predict([[4, 4, 3, 3]])
+
+#####
+y_pred = clf_gini.predict(X_test)
+print(y_pred)
+#####
+
+#####
+y_pred_en = clf_entropy.predict(X_test)
+print(y_pred_en)
+#####
+
+print("Accuracy is ", accuracy_score(y_test, y_pred_en)*100)
